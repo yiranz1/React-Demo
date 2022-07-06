7d5506fd12da015bfbaa6d0fab20ff4e00a4b112
yiranz1 <yrzhou01@gmail.com>
2022-07-05 22:56:54 -0500

    Pass information using props

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -7,50 +7,54 @@ const welcome = {
 
 const numbers = [1, 2, 3, 4];
 const exponentialNumbers = numbers.map((number) => number * number);
-const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org',
-        author: 'Jordan Walke',
-        points: 5,
-        num_comments: 3,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org',
-        author: 'Dan',
-        points: 6,
-        num_comments: 2,
-        objectID: 1,
-    }
-]
+
 
 const App = () => {
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org',
+            author: 'Jordan Walke',
+            points: 5,
+            num_comments: 3,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org',
+            author: 'Dan',
+            points: 6,
+            num_comments: 2,
+            objectID: 1,
+        }
+    ]
+
   return (
    <div>
      <h1>{welcome.greeting}, {welcome.title}</h1>
        <Search />
        <div>Exponential Numbers: {JSON.stringify(exponentialNumbers)}</div>
-       <List />
+       <List list={stories} />
    </div>
   );
 }
 
-const List = () => {
+const List = (props) => {
     return (
         <ul>
-            {list.map((item) => {
-                return (
-                    <li key={item.objectID}>
-                        <span><a href={item.url}>{item.title}</a></span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                    </li>
-                )
-            })}
+            {props.list.map((item) => <Item item={item} key={item.objectID} />)}
         </ul>
+    );
+}
+
+const Item = (props) => {
+    return (
+        <li>
+            <span><a href={props.item.url}>{props.item.title}</a></span>
+            <span>{props.item.author}</span>
+            <span>{props.item.num_comments}</span>
+            <span>{props.item.points}</span>
+        </li>
     );
 }
 
