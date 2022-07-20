7b2a4155e52dcf7ddd14134a4ac7b82ac21663de
yiranz1 <yrzhou01@gmail.com>
2022-07-19 23:07:14 -0500

    Props destructuring

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -54,22 +54,23 @@ const List = (props) => {
     );
 }
 
-const Item = (props) => {
+const Item = ({ item }) => {
     return (
         <li>
-            <span><a href={props.item.url}>{props.item.title}</a></span>
-            <span>{props.item.author}</span>
-            <span>{props.item.num_comments}</span>
-            <span>{props.item.points}</span>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
         </li>
     );
 }
 
 const Search = (props) => {
+    const { search, onSearch } = props;
     return (
         <div>
             <label htmlFor="search">Search: </label>
-            <input id="search" type="text" value={props.search} onChange={props.onSearch} />
+            <input id="search" type="text" value={search} onChange={onSearch} />
         </div>
     );
 }
