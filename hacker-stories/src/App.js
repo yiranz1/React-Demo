392c52fe5d2493302c4eab6b0792a684136fd3f5
yiranz1 <yrzhou01@gmail.com>
2022-10-04 22:42:54 -0500

    Add a submit button to fetch data from API

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -59,13 +59,11 @@ const App = () => {
         storiesReducer,
         {data: [], isLoading: false, isError: false}
     );
+    const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
 
     const handleFetchStories = React.useCallback(() => {
-        if (!searchTerm) {
-            return;
-        }
         dispatchStories({ type: 'STORIES_FETCH_INIT' });
-        fetch(`${API_ENDPOINT}${searchTerm}`)
+        fetch(url)
             .then((response) => response.json())
             .then((result) => {
                 dispatchStories({
@@ -76,7 +74,7 @@ const App = () => {
             .catch(() => {
                 dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
             });
-    }, [searchTerm]);
+    }, [url]);
 
     React.useEffect(() => {
         handleFetchStories();
@@ -89,9 +87,13 @@ const App = () => {
         });
     }
 
-    const handleSearch = (event) => {
+    const handleSearchInput = (event) => {
         setSearchTerm(event.target.value);
         console.log(searchTerm);
+    }
+
+    const handleSearchSubmit = () => {
+        setUrl(`${API_ENDPOINT}${searchTerm}`);
     }
 
   return (
@@ -100,10 +102,17 @@ const App = () => {
        <InputWithLabel
            id="search"
            value={searchTerm}
-           onInputChange={handleSearch}
+           onInputChange={handleSearchInput}
        >
            <strong>Search: </strong>
        </InputWithLabel>
+       <button
+           type="button"
+           disabled={!searchTerm}
+           onClick={handleSearchSubmit}
+       >
+           Submit
+       </button>
        <div>Exponential Numbers: {JSON.stringify(exponentialNumbers)}</div>
        {stories.isError && <p>Something went wrong...</p>}
        {stories.isLoading ? (<p>Loading...</p>) :
