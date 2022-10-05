1d0ef1583916cd56b5c0a885d69f77fc6b5077bb
yiranz1 <yrzhou01@gmail.com>
2022-10-04 22:32:00 -0500

    Memoized functions

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -60,7 +60,7 @@ const App = () => {
         {data: [], isLoading: false, isError: false}
     );
 
-    React.useEffect(() => {
+    const handleFetchStories = React.useCallback(() => {
         if (!searchTerm) {
             return;
         }
@@ -77,6 +77,10 @@ const App = () => {
                 dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
             });
     }, [searchTerm]);
+
+    React.useEffect(() => {
+        handleFetchStories();
+    }, [handleFetchStories]);
 
     const handleRemoveStory = (item) => {
         dispatchStories({
