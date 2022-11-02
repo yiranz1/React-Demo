ac31e2d37cd8a32151b1042ee4121c643c9d1ebe
yiranz1 <yrzhou01@gmail.com>
2022-11-01 23:53:17 -0500

    Add tests, for some reason config does not work

--- hacker-stories/src/App.test.js
+++ hacker-stories/src/App.test.js
@@ -29,5 +29,20 @@ const storyTwo = {
 const stories = [storyOne, storyTwo];
 
 describe('storiesReducer', () => {
+  test('removes a story from all stories', () => {
+    const action = {
+      type: 'REMOVE_STORY',
+      payload: storyOne,
+    };
+    const state = { data: stories, isLoading: false, isError: false };
 
+    const newState = storiesReducer(state, action);
+    const expectedState = {
+      data: [storyTwo],
+      isLoading: false,
+      isError: false,
+    };
+
+    expect(newState).toEqual(expectedState);
+  });
 });
