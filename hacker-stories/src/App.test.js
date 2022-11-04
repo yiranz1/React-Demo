51ee2db0040bf5897cb6128ff9794e0e6b098ed7
yiranz1 <yrzhou01@gmail.com>
2022-11-03 23:24:43 -0500

    Add tests for SearchForm component

--- hacker-stories/src/App.test.js
+++ hacker-stories/src/App.test.js
@@ -65,3 +65,31 @@ describe("Item", () => {
     expect(handleRemoveItem).toHaveBeenCalledTimes(1);
   });
 });
+
+describe("SearchForm", () => {
+  const searchFormProps = {
+    searchTerm: 'React',
+    onSearchInput: jest.fn(),
+    onSearchSubmit: jest.fn(),
+  };
+
+  test("renders the input field with its value", () => {
+    render(<SearchForm {...searchFormProps} />);
+    expect(screen.getByDisplayValue('React')).toBeInTheDocument();
+    expect(screen.getByLabelText(/Search/)).toBeInTheDocument();
+  });
+
+  test("calls onSearchInput on input field change", () => {
+    render(<SearchForm {...searchFormProps} />);
+    fireEvent.change(screen.getByDisplayValue("React"), {
+      target: { value: "Redux" }
+    });
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+  });
+
+  test("calls onSearchSubmit on button submit click", () => {
+    render(<SearchForm {...searchFormProps} />);
+    fireEvent.submit(screen.getByRole('button'));
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+  });
+});
