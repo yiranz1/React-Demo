f3bd32e3876a965c428e69613ef719164060251d
yiranz1 <yrzhou01@gmail.com>
2022-11-30 23:21:12 -0600

    Add title for each data field

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -216,6 +216,13 @@ const List = React.memo(({ list, onRemoveItem }: ListProps) => {
     console.log("B: InputWithLabel");
     return (
         <ul>
+            <li style={{ display: 'flex' }}>
+                <span style={{ width : '40%' }}>Title</span>
+                <span style={{ width : '30%' }}>Author</span>
+                <span style={{ width : '10%' }}>Comments</span>
+                <span style={{ width : '10%' }}>Points</span>
+                <span style={{ width : '10%' }}>Actions</span>
+            </li>
             {list.map((item) => <Item item={item} key={item.objectID} onRemoveItem={onRemoveItem} />)}
         </ul>
     );
@@ -228,7 +235,7 @@ type ItemProps = {
 
 const Item = ({ item, onRemoveItem } : ItemProps) => {
     return (
-        <li className="item">
+        <li className="item" style={{ display: 'flex' }}>
             <span style={{ width: '40%' }}><a href={item.url}>{item.title}</a></span>
             <span style={{ width: '30%' }}>{item.author}</span>
             <span style={{ width: '10%' }}>{item.num_comments}</span>
