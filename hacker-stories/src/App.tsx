155f6c070cff2f9ed1356c03b0bf0072d19ee18d
yiranz1 <yrzhou01@gmail.com>
2022-12-06 22:55:08 -0600

    Add reverse search

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -223,13 +223,17 @@ type ListProps = {
 }
 const List = React.memo(({ list, onRemoveItem }: ListProps) => {
     console.log("B: InputWithLabel");
-    const [sort, setSort] = React.useState('NONE');
+    const [sort, setSort] = React.useState({
+        sortKey: 'NONE',
+        isReverse: false,
+    });
     const handleSort = (sortKey) => {
-        setSort(sortKey);
+        const isReverse = sort.sortKey === sortKey && !sort.isReverse;
+        setSort({sortKey, isReverse});
     };
 
-    const sortFunction = SORTS[sort];
-    const sortedList = sortFunction(list);
+    const sortFunction = SORTS[sort.sortKey];
+    const sortedList = sort.isReverse ? sortFunction(list).reverse() : sortFunction(list);
 
     return (
         <ul>
